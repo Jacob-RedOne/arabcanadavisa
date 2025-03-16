acb868ea9817b9bbebec3ada8c611103a30b9b12
Jacob-RedOne <archredone000@gmail.com>
2025-03-15 22:20:33 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("loginForm").addEventListener("submit", function(event) {
         event.preventDefault(); // Prevents form from actually submitting
 
-        // Show error message
+        // Show error message only after the user tries to log in
         document.getElementById("errorMessage").style.display = "block";
     });
 });
