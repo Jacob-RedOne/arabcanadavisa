d2a0671951afb64c7f93e1250fb89f5234167202
Jacob-RedOne <archredone000@gmail.com>
2025-03-15 22:08:56 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-console.log("JavaScript loaded!");
+// Wait for the document to load before adding event listeners
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent the form from actually submitting
+
+        // Show the error message
+        document.getElementById("errorMessage").style.display = "block";
+    });
+});
